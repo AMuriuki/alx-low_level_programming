cc8d286a0173e63efed2646a7b2a3976841403b1
Arnold Nderitu <arnoldnderitu@gmail.com>
2022-10-19 09:42:36 -0700

    print all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	char c;
+
+	for (d = 0; d < 10; d++)
+	{
+		putchar(d + '0');
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
