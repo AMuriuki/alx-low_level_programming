176e6e7198c131f066047a92ad84f9c25667db07
Arnold Nderitu <arnoldnderitu@gmail.com>
2022-10-19 11:03:44 -0700

    print out all possible combinations of two digits  seperated by a comma

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		putchar(c + '0');
+		putchar(d + '0');
+
+		if (c == 8 && d == 9)
+		{
+			continue;
+		}
+		else 
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		if (d == 9)
+		{
+			c++;
+			d = c;
+		}
+	}	
+	putchar('\n');
+	return (0);
+}
