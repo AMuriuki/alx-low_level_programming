6fe92a4673cebd086f93d4e0586438e50289661a
Arnold Nderitu <arnoldnderitu@gmail.com>
2022-10-20 03:30:14 -0700

    remove trailing whitespaces

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints _putchar
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("_putchar\n");
+	char h[] = "_putchar";
+	int m;
+
+	m = 0;
+
+	while (m <= 7)
+	{
+		_putchar(h[m]);
+		m++;
+	}
+	_putchar('\n');
 	
 	return (0);
 }
