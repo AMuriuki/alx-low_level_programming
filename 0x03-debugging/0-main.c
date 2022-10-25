655139a34c46a03f4e2af698f46f2e0a0b1c977b
Arnold Nderitu <arnoldnderitu@gmail.com>
2022-10-25 00:36:14 -0700

    increase lines

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,13 @@
  * main - test the function that print if integer is positive or negative
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
