aa25c0a0469bf5d527af87c96ff1b1d9348992f2
Arnold Nderitu <arnoldnderitu@gmail.com>
2022-10-25 11:47:00 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ *
+ * @s: input char type argument
+ */
+void rev_string(char *s)
+{
+	int i, k, tmp, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	len = i;
+	i--;
+	while (k < len / 2)
+	{
+		tmp = s[k];
+		s[k] = s[i];
+		s[i] = tmp;
+		i--;
+		k++;
+	}
+}
