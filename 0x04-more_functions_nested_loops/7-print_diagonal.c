c45064849d89c26b3c7f8217b35cba03952f3b91
Arnold Nderitu <arnoldnderitu@gmail.com>
2022-10-26 03:54:41 -0700

    fix bug

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,9 +14,9 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 	}
-	for (c = 0; c < n; a++)
+	for (c = 0; c < n; c++)
 	{
-		for (d = 0; d < a; d++)
+		for (d = 0; d < c; d++)
 		{
 			_putchar(' ');
 		}
