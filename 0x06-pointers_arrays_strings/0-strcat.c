6d709f12b1db2d6e6123ad7c307e9741e84ac5ff
Arnold Nderitu <arnoldnderitu@gmail.com>
2022-10-29 13:18:40 -0700

    concatenates two strings. appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings.
+ *
+ * @dest: a pointer to a string
+ * @src: a pointer to another string
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	for (j = 0; dest[j] != '\0'; j++)
+		;
+	while (src[i] != '\0')
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+	return (dest);
+}
